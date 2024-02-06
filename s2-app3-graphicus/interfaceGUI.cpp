9c3758fbe99537eebca7d1e63b30256e012694c8
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2024-02-05 19:53:41 -0500

    Inclusion de GraphicusGUI dans le projet

--- s2-app3-graphicus/interfaceGUI.cpp
+++ s2-app3-graphicus/interfaceGUI.cpp
@@ -1,2 +1,10 @@
+/********
+ * Fichier: interfaceGUI.cpp
+ * Auteurs: Vincent Simard-Schmidt (simv2104) & Kevin Gauvin (gauk1703)
+ * Date: 05 fevrier 2024 (creation)
+ * Description: Implementation des methodes de la classe InterfaceGUI.h.
+********/
+
 #include "interfaceGUI.h"
 
+InterfaceGUI::InterfaceGUI(const char* titre) : GraphicusGUI(titre) {}
--- s2-app3-graphicus/s2-app3-graphicus.cpp
+++ s2-app3-graphicus/s2-app3-graphicus.cpp
@@ -1,9 +1,15 @@
+#include <QApplication>
+#include "interfaceGUI.h"
 #include "tests.h"
 
-int main()
+int main(int argc, char** argv)
 {
-	Tests tests;
-	tests.tests_unitaires();
-	tests.tests_validation();
-	system("pause");
+	//Tests tests;
+	//tests.tests_unitaires();
+	//tests.tests_validation();
+	//system("pause");
+
+	QApplication app(argc, argv);
+	InterfaceGUI i ("Graphicus-03 (GraphicusGUI)");
+	return app.exec();
 }
